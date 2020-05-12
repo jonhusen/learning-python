42331f455e2d53f26e0331eb0ff529e1d7560f22
Jon Husen <jon.husen@gmail.com>
2020-05-12 16:15:21 -0400

    refactor with multiple return values

--- pluralsight/corepy/exceptional.py
+++ pluralsight/corepy/exceptional.py
@@ -14,12 +14,10 @@ DIGIT_MAP = {
 
 def convert(s):
     """Convert a string to an integer."""
-    x = -1
     try:
         number = ""
         for token in s:
             number += DIGIT_MAP[token]
-        x = int(number)
+        return int(number)
     except (KeyError, TypeError):
-        pass
-    return x
+        return -1
