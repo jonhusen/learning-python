765681cb3408a6d4c2edde8800bf29d4169ba4b0
Jon Husen <jon.husen@gmail.com>
2020-05-13 16:24:51 -0400

    class invariants

--- pluralsight/corepy/airtravel.py
+++ pluralsight/corepy/airtravel.py
@@ -3,6 +3,15 @@
 
 class Flight:
     def __init__(self, number):
+        if not number[:2].isalpha():
+            raise ValueError(f"No airline code in '{number}'")
+
+        if not number[:2].isupper():
+            raise ValueError(f"Invalid airline code '{number}'")
+
+        if not (number[2:].isdigit() and int(number[2:]) <= 9999):
+            raise ValueError(f"Invalid route number '{number}'")
+
         self._number = number
         # Underscore avoids name clash with method of same name
         # convention - implementation details not intended for
@@ -10,3 +19,6 @@ class Flight:
 
     def number(self):
         return self._number
+
+    def airline(self):
+        return self._number[:2]
