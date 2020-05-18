f30f983b73c69449d463f5a121307afad26ddf0d
Jon Husen <jon.husen@gmail.com>
2020-05-17 23:03:51 -0400

    inheritance and abstract base classes; re-implement Aircraft as abc

--- pluralsight/corepy/airtravel.py
+++ pluralsight/corepy/airtravel.py
@@ -110,13 +110,24 @@ class Flight:
                     yield passenger, f"{row}{letter}"
 
 
-class AirbusA319:
+class Aircraft:
+    """Abstract base class
+    self.seating_plan() does not exist in this class.
+    This class must be inherited by another class to be useful.
+    """
+
     def __init__(self, registration):
         self._registration = registration
 
     def registration(self):
         return self._registration
 
+    def num_seats(self):
+        rows, row_seats = self.seating_plan()
+        return len(rows) * len(row_seats)
+
+
+class AirbusA319(Aircraft):
     def model(self):
         return "Airbus A319"
 
@@ -124,13 +135,7 @@ class AirbusA319:
         return range(1, 23), "ABCDEF"
 
 
-class Boeing777:
-    def __init__(self, registration):
-        self._registration = registration
-
-    def registration(self):
-        return self._registration
-
+class Boeing777(Aircraft):
     def model(self):
         return "Boeing 777"
 
