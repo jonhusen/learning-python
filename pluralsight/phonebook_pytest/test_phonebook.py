ffbc60abc588634ee7ead7e419d9460d300d8ed9
Jon Husen <jon.husen@gmail.com>
2020-06-08 23:53:44 -0400

    examples using pytest

--- pluralsight/phonebook_pytest/test_phonebook.py
+++ pluralsight/phonebook_pytest/test_phonebook.py
@@ -1,5 +1,33 @@
+import pytest
+
+
+class Phonebook():
+
+    def __init__(self) -> None:
+        self.numbers = {}
+
+    def add(self, name, number):
+        self.numbers[name] = number
+
+    def lookup(self, name):
+        return self.numbers[name]
+
+    def names(self):
+        return set(self.numbers.keys())
+
 
 def test_lookup_by_name():
     phonebook = Phonebook()
     phonebook.add("Bob", "1234")
     assert "1234" == phonebook.lookup("Bob")
+
+
+def test_phonebook_contains_all_names():
+    phonebook = Phonebook()
+    phonebook.add("Bob", "1234")
+    assert "Bob" in phonebook.names()
+
+def test_missing_name_raises_error():
+    phonebook = Phonebook()
+    with pytest.raises(KeyError):
+        phonebook.lookup("Bob")
