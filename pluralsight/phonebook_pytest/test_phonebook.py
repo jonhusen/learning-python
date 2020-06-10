5e9efed6ac0c1e29cf74f12f7dafb64ce468abb5
Jon Husen <jon.husen@gmail.com>
2020-06-10 10:33:01 -0400

    example of tear down fixture in pytest using yield statement

--- pluralsight/phonebook_pytest/test_phonebook.py
+++ pluralsight/phonebook_pytest/test_phonebook.py
@@ -1,33 +1,24 @@
 import pytest
 
-
-class Phonebook():
-
-    def __init__(self) -> None:
-        self.numbers = {}
-
-    def add(self, name, number):
-        self.numbers[name] = number
-
-    def lookup(self, name):
-        return self.numbers[name]
-
-    def names(self):
-        return set(self.numbers.keys())
+from pluralsight.phonebook_pytest.phonebook import Phonebook
 
 
-def test_lookup_by_name():
+@pytest.fixture
+def phonebook():
     phonebook = Phonebook()
+    yield phonebook
+    phonebook.clear()
+
+
+def test_lookup_by_name(phonebook):
     phonebook.add("Bob", "1234")
     assert "1234" == phonebook.lookup("Bob")
 
 
-def test_phonebook_contains_all_names():
-    phonebook = Phonebook()
+def test_phonebook_contains_all_names(phonebook):
     phonebook.add("Bob", "1234")
     assert "Bob" in phonebook.names()
 
-def test_missing_name_raises_error():
-    phonebook = Phonebook()
+def test_missing_name_raises_error(phonebook):
     with pytest.raises(KeyError):
         phonebook.lookup("Bob")
